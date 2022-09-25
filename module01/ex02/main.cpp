3fe005fa0a139391575ad7d68f91f9a413bf8ecc
jaka72 <jaka72@gmail.com>
2022-09-25 19:38:01 +0200

    Cleaned mod01/02

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-	//  REFERENCE
 	std::string str1 = "HI THIS IS BRAIN";
 
 	std::string *stringPTR = &str1;     // POINTER
@@ -11,18 +10,18 @@ int main()
 	// std::string *asd;
 	// 	asd = &str1;
 	
-	std::cout << "str1:                " << str1 << "\n";           // STRING VALUE
-	std::cout << "The address of str1: " << &str1 << "\n\n";		// THE ADRES
+	std::cout << " str1:                " << str1 << "\n";           // STRING VALUE
+	std::cout << "&str1:                " << &str1 << "     (address of str1)\n\n";		// THE ADRES
 	
-	std::cout << "stringREF:           " << stringREF << "\n";      // A REFERENCE CAN BE NORMALY PRINTED AS A STRING
-	std::cout << "&stringREF:          " << &stringREF << "\n\n";   //   PRINTS THE ADRES OF THE STRING
-//  std::cout << "*stringREF:          " << *stringREF << "\n\n";   //   ERROR
+	std::cout << " stringREF:           " << stringREF << "\n";      // A REFERENCE CAN BE NORMALY PRINTED AS A STRING
+	std::cout << "&stringREF:           " << &stringREF << "     (reference, same as the address of str1)\n";   //   PRINTS THE ADRES OF THE STRING
+	std::cout << "*stringREF:           " << "(* of reference not possible)\n\n";   //   ERROR
 
 
-	std::cout << "stringPTR:           " << stringPTR << "\n";    // POINTER, THE ADDRESS OF str1
-	std::cout << "   *stringPTR        " << *stringPTR << "\n";
-	std::cout << "   &stringPTR        " << &stringPTR << "\n";
-	std::cout << "   &(*stringPTR)     " << &(*stringPTR) << "\n";
-	std::cout << "   &(*&(*stringPTR))) " << &(*(&(*stringPTR))) << "\n\n";
+	std::cout << "     *stringPTR            " << *stringPTR << "\n";
+	std::cout << "      stringPTR:           " << stringPTR << "     (address of str1)\n";    // POINTER, THE ADDRESS OF str1
+	std::cout << "     &stringPTR            " << &stringPTR << "     (address of the pointer)\n";
+	std::cout << "   &(*stringPTR)           " << &(*stringPTR) << "\n";
+	std::cout << "&(*&(*stringPTR)))         " << &(*(&(*stringPTR))) << "\n\n";
 
 }
