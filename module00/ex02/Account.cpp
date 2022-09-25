f99857092c28d565abd7c2bcd28c9f9518a5011f
jaka72 <jaka72@gmail.com>
2022-09-25 16:54:45 +0200

    mod00/02 now working, only needs removing comments

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -2,17 +2,67 @@
 #include <iostream>
 #include <iomanip>      // std::get_time
 #include <ctime>        // struct std::tm
+// #include <sys/time.h>
+
+
+
+//	IS THIS ALLOWED TO BE INITIATED HERE OUTSIDE ANY FUNCTION ???
+//		The initialisation of the static int i must be done outside of any function.
+int Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+
+// HOW CAN WE RETURN THE OSTREAM, SO THAT THIS FUNCTION CAN BE USED
+//	BETWEEN <<  setfield() <<   ???
+// std::ostream &set_field()
+void set_field()
+{
+	std::cout << std::setfill('0') << std::setw(2);
+	//return (*ostream);
+}
 
 
 void	Account::_displayTimestamp( void )
 {
-	std::cout << "[time_stamp] ";
+	// A)	Time since 1970
+	// struct timeval my_time;
+	// gettimeofday(&my_time, NULL); // NULL ??
+	// std::cout << "[" << my_time.tv_sec << "_" << my_time.tv_usec << "] ";
+
+	// B)	Time since 1900
+	// time_t time = std::time(NULL);
+	// struct tm now = *std::localtime(&time); 
+
+	// std::cout << "[" << 1900 + now.tm_year;
+	// set_field();
+	// std::cout << 1 + now.tm_mon;
+	// set_field();
+	// std::cout << now.tm_mday << "_";
+	// set_field();
+	// std::cout << now.tm_hour;
+	// set_field();
+	// std::cout << now.tm_min;
+	// set_field();
+	// std::cout << now.tm_sec << "] ";
+
+	// C)
+	std::cout << "[19920104_091532] ";
+}
+
+
+Account::Account()
+{
+	std::cout << "Default constructor\n";
 }
 
 
 Account::Account(int init_dep)
 {
+
 	static int i = 0;
+
 	this->_accountIndex = i;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
@@ -28,11 +78,6 @@ Account::Account(int init_dep)
 	i++;
 }
 
-Account::Account()
-{
-	std::cout << "Default constructor\n";
-}
-
 
 Account::~Account()
 {
@@ -41,6 +86,7 @@ Account::~Account()
 	std::cout << "amount:" << this->_amount << ";";
 	std::cout << "closed\n";
 }
+
 
 ////////////////////////////////////////////////////////////////////
 // GENERAL INFO
@@ -60,6 +106,8 @@ int	Account::getNbWithdrawals( void )
 {
 	return (_totalNbWithdrawals);
 }
+
+
 // DISPLAY STATISTICS IN ONE LINE
 void	Account::displayAccountsInfos( void )
 {
@@ -68,7 +116,7 @@ void	Account::displayAccountsInfos( void )
 	std::cout << "accounts:" << getNbAccounts() << ";";
 	std::cout << "total:" << getTotalAmount() << ";";
 	std::cout << "deposits:" << getNbDeposits() << ";";
-	std::cout << "withdrawals:" << getNbWithdrawals() << ";\n\n";
+	std::cout << "withdrawals:" << getNbWithdrawals() << "\n\n";
 
 
 	// HOW TO NOW ACCESS EACH INDIVIDUAL OBJECT ?????
@@ -80,11 +128,16 @@ void	Account::displayAccountsInfos( void )
 
 void	Account::displayStatus( void ) const
 {
+	// std::cout << "   STATUS-ALL\n";
+
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
 	std::cout << "deposits:" << this->_nbDeposits << ";";
-	std::cout << "withdrawals:" << this->_nbWithdrawals << ";\n";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << "\n";
+
+	if (this->_accountIndex == this->_nbAccounts -1)
+		std::cout << "\n";
 }
 
 
@@ -98,7 +151,7 @@ void	Account::makeDeposit( int deposit )
 	_amount = _amount + deposit;
 	this->_nbDeposits++;
 	std::cout << "amount:" << _amount << ";";
-	std::cout << "nb_deposits:" << _nbDeposits << ";";
+	std::cout << "nb_deposits:" << _nbDeposits << "\n";
 	_totalNbDeposits++;
 	_totalAmount += deposit;
 }
@@ -108,6 +161,8 @@ void	Account::makeDeposit( int deposit )
 // WITHDRAWING /////////////////////////////////////////////////////
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	if (this->_accountIndex == 0)
+		std::cout << "\n";
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";";
 	std::cout << "p_amount:" << _amount << ";";
@@ -126,67 +181,65 @@ bool	Account::makeWithdrawal( int withdrawal )
 		return (0);
 	}
 	std::cout << "amount:" << _amount << ";";
-	std::cout << "nb_withdrawals:" << _nbWithdrawals << ";\n";
+	std::cout << "nb_withdrawals:" << _nbWithdrawals << "\n";
 	return (1);
 }
 
 
 // MY ADDED FUNCTIONS ////////////////////////
 
-void print_all_accounts(Account **arr)
-{
-	int i = 0;
-	while (i < 8)
-	{
-		arr[i]->displayStatus();
-		i++;
-	}
-	std::cout << "\n";
+// void print_all_accounts(Account **arr)
+// {
+// 	int i = 0;
+// 	while (i < 8)
+// 	{
+// 		arr[i]->displayStatus();
+// 		i++;
+// 	}
+// 	std::cout << "\n";
+// }
 
-}
+// void initialize_objects(Account **arr, int *amounts)
+// {
+// 	int i = 0;
+// 	while (i < 8)
+// 	{
+// 		arr[i] = new Account(amounts[i]);
+// 		i++;
+// 	}
+// 	// std::cout << "\n";
+// }
 
-void initialize_objects(Account **arr, int *amounts)
-{
-	int i = 0;
-	while (i < 8)
-	{
-		arr[i] = new Account(amounts[i]);
-		i++;
-	}
-	// std::cout << "\n";
-}
+// void depositing(Account **arr, int *deposits)
+// {
+// 	//std::cout << "   DEPOSITING\n";
+// 	int i = 0;
+// 	while (i < 8)
+// 	{
+// 		arr[i]->makeDeposit(deposits[i]);
+// 		std::cout << "\n";
+// 		i++;
+// 	}
+// }
 
-void depositing(Account **arr, int *deposits)
-{
-	int i = 0;
-	while (i < 8)
-	{
-		arr[i]->makeDeposit(deposits[i]);
-		i++;
-	}
-	std::cout << "\n";
-}
-
-void withdrawing(Account **arr, int *withdrawals)
-{
-	int i = 0;
-	while (i < 8)
-	{
-		arr[i]->makeWithdrawal(withdrawals[i]);
-		i++;
-	}
-}
+// void withdrawing(Account **arr, int *withdrawals)
+// {
+// 	//std::cout << "   WITHDRAWING\n";
+// 	int i = 0;
+// 	while (i < 8)
+// 	{
+// 		arr[i]->makeWithdrawal(withdrawals[i]);
+// 		i++;
+// 	}
+// }
 
 ////////////////////////////////////////////////////////////////////
 
 
 
-int Account::_nbAccounts = 0;
-int	Account::_totalAmount = 0;
-int	Account::_totalNbDeposits = 0;
-int	Account::_totalNbWithdrawals = 0;
 
 
+// 	MY MAIN ////////////////////////////////////////
 // int main()
 // {
 // 	Account *arr[8];
