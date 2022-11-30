60bededea75a2239a354a520935bccc000bb0565
jaka72 <jaka72@gmail.com>
2022-11-30 19:16:04 +0100

    Added Class creator ccf. Started ex00 for examo05

---
+++ exam_rank05/coplien_form_maker/source/Test.cpp
@@ -0,0 +1,35 @@
+#include "Test.hpp"
+
+//Default constructor
+Test::Test()
+{
+
+}
+
+//Param. constructor
+
+//Copy constructor
+Test::Test(Test const &other)
+{
+
+}
+
+//Destructor
+Test::~Test()
+{
+
+}
+
+//Overload operator=
+Test &Test::operator=(Test const &other)
+{
+
+	return(*this);
+}
+
+//Getters
+
+//Setters
+
+//Public member functions
+
---
+++ exam_rank05/coplien_form_maker/source/Test.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+/*
+**==========================
+**		Includes
+**==========================
+*/
+
+
+class Test
+{
+	private:
+
+	public:
+		//Constructors
+		Test();
+		Test(Test const &other);
+		~Test();
+
+		//Overload operator=
+		Test &operator=(Test const &other);
+
+		//Getters
+
+		//Setters
+
+		//Public member functions
+
+};
